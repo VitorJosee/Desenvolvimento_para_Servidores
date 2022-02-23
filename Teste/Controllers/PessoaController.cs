97d620c5190679e2671bcd03b0ba7a6ddf0cf7b5
0220832111009 <0220832111009@LAB4TC17>
2022-02-23 09:54:15 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Controllers/PessoaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Teste.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public static List<Pessoa> lsPessoas = new List<Pessoa>();
+        public IActionResult Index()
+        {
+            return View(lsPessoas);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult salvar(Pessoa c)
+        {
+            lsPessoas.Add(c);
+            return RedirectToAction("index");
+        }
+    }
+}
